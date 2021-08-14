16bc17d32e88f6e0e33da91ed470c2cdbff6edcd
valquiriav <valvitoli@gmail.com>
2021-08-14 11:27:04 -0300

    Stream

--- src/br/com/letscode/Main.java
+++ src/br/com/letscode/Main.java
@@ -3,21 +3,18 @@ package br.com.letscode;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<Curso> cursos = new ArrayList<Curso>();
+        cursos.add(new Curso("Python", 45));
+        cursos.add(new Curso("JavaScript", 150));
+        cursos.add(new Curso("Java 8", 113));
+        cursos.add(new Curso("C", 55));
 
-        List<String> fruits = new ArrayList<String>();
-
-        fruits.add("apple");
-        fruits.add("pineapple");
-        fruits.add("grape");
-
-        fruits.sort(Comparator.comparing(String::length));
-
-        fruits.forEach(System.out::println);
-
+        cursos.stream()
+        .filter(c -> c.getAlunos() > 50)
+        .forEach(c -> System.out.println(c.getNome()));
     }
 }
